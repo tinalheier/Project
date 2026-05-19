b5bcc55a00aab33e4353379cdaf8ea8f68fb9e99
tinalhe <tinalhe@stud.ntnu.no>
2026-05-19 10:54:18 +0200

    Fixe DTM path to render RENDER

--- Backend/rasterfiles.py
+++ Backend/rasterfiles.py
@@ -1,16 +1,22 @@
-
 import os
 import rasterio
 from rasterio.merge import merge
 from shapely.geometry import LineString, box
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(BASE_DIR)
 
-def combine_tifs(startPoint, endPoint, folder_path = "/Backend/data/DTM_nasjonal/", buffer = 10000):
+DTM_FOLDER = os.path.join(PROJECT_ROOT, "data", "DTM_nasjonal")
 
-    #finner alle .tif filer i mappen
-    tif_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".tif")]
 
+def combine_tifs(startPoint, endPoint, folder_path=DTM_FOLDER, buffer=10000):
+
+    tif_files = [
+        os.path.join(folder_path, f)
+        for f in os.listdir(folder_path)
+        if f.endswith(".tif")
+    ]
 
     line = LineString([startPoint, endPoint])
     buffer_geo = line.buffer(buffer)
@@ -20,16 +26,15 @@ def combine_tifs(startPoint, endPoint, folder_path = "/Backend/data/DTM_nasjonal
     for raster in tif_files:
         src = rasterio.open(raster)
         boundingbox = box(*src.bounds)
-        
+
         if boundingbox.intersects(buffer_geo):
             rasters.append(src)
-        
         else:
             src.close()
-    
+
     if not rasters:
         raise ValueError("No overlap")
-    
+
     mosaic, transform = merge(rasters)
 
     meta = rasters[0].meta.copy()
@@ -38,12 +43,10 @@ def combine_tifs(startPoint, endPoint, folder_path = "/Backend/data/DTM_nasjonal
         "width": mosaic.shape[2],
         "transform": transform
     })
- 
 
-    BASE_DIR = os.path.dirname(__file__)
-    out_path = os.path.join(BASE_DIR, "data", "merged_raster.tif")
+    out_path = os.path.join(PROJECT_ROOT, "data", "merged_raster.tif")
 
-    os.makedirs(os.path.dirname(out_path), exist_ok=True)   
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
 
     with rasterio.open(out_path, "w", **meta) as dst:
         dst.write(mosaic)
